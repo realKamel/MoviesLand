2d179b0603a9d20529fcf23cabbb44fc92bc353a
abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-03 15:54:17 +0300

    init files

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import Search from "./components/search";
+import { useEffect, useState } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  header: {
+    accept: "application/json",
+    authorization: `Bearer ${API_KEY}`,
+  },
+};
+const App = () => {
+  //states
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    try {
+      setIsLoading(true);
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to Fetch Movies");
+      } else {
+        const data = await response.json();
+        if (data.Response === "False") {
+          setErrorMessage(data.Error || "Failed to Fetch Movies");
+          setMovieList([]);
+          return;
+        }
+        setMovieList(data.results || []);
+      }
+    } catch (error) {
+      console.log(`Error Fetching Movies ${error}`);
+      setErrorMessage("Error fetching movies please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
