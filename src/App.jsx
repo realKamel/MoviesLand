1ff7ecfc9d6111120eceeab0a70a483c7fe3f298
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-05 13:38:42 +0300

    Fix paths for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
       <div className="pattern"></div>
       <div className="wrapper">
         <header>
-          <img src="/hero.png" alt="Hero Banner" />
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without Hassle
